ae5a1b02c7a042a26f431afc51b31668bfae9f72
mehmet <tas349873@gmail.com>
2022-06-12 00:55:37 +0300

    v3

---
+++ assignment-3.js
@@ -0,0 +1,7 @@
+//!ÖDEVLER
+// ÖDEV1:
+// fiyatlar dizisinde her bir fiyata %10 zam yapalım. NOT: FOREACH kullanılmalı ve orjinal dizi değiştirilmeli.
+
+const fiyatlar = [100, 250, 50, 89];
+
+console.log(fiyatlar.forEach((meta) => meta+(meta/100)*10));
