37ec7063818eed5fc6750b68b5964fd8dd23c58d
mehmet <tas349873@gmail.com>
2022-06-12 00:02:31 +0300

    v1

---
+++ assignment.js
@@ -0,0 +1,37 @@
+// ÇALIŞMA SORULARI
+// SORU1: Taban ve yükseklik değerlerini parametre olarak alan ve bir üçgenin
+// alanını hesaplayarak ana programa döndüren fonksiyonu yazınız.
+
+
+// SORU2: kareAl, küpAl, üsAl şeklinde üç adet farklı arrow fonksiyonu
+// tanımlayın. Bu fonksiyonların ana programdan gereken parametreleri alarak
+// sonuçları ana programa döndürmeli gerekmektedir.
+
+
+// SORU3: Yıl değerini parametre olarak alan ve bu yılın artık yıl olup olmadığını
+// hesaplayarak sonucu ana programa döndüren fonksiyonu function-expression
+// yöntemi ile yazınız. NOT: Yıl 4’e tam bölünüyorsa VE (100'e tam bölünmüyorsa
+// VEYA 400'e tam bölünüyorsa) artık yıldır aksi takdirde değildir.
+
+//! 1.soru
+
+//! 1.yöntem
+// const alan = function(a,h){
+//     let x = (a*h)/2;
+//     return x
+// }
+// console.log(alan(6,12));
+
+//! 2.yöntem
+// const alan = function(a,h){
+//     let x = (a*h)/2;
+//    console.log(x)
+// }
+// console.log(alan(6,12));
+
+
+//! 3.yöntem
+
+// const alan = (a,h) => (a*h)/2;
+
+// console.log(alan(4,6));
