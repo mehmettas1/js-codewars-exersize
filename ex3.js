b6efa623ebb7e4e39a42c86c8393c346665b17a6
mehmet <tas349873@gmail.com>
2022-06-18 09:22:36 +0300

    v1

---
+++ ex3.js
@@ -0,0 +1,4 @@
+//!Write a Python/JavaScript code to sort the list/array below without using .sort() method.
+// elements of list = [999, 333, 2, 8982, 12, 45, 77, 99, 11]
+// Expected output:
+// [2, 11, 12, 45, 77, 99, 333, 999, 8982]
