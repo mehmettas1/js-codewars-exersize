8b62ac00ce3002d4293c665e734f34a99081df52
mehmet <tas349873@gmail.com>
2022-06-22 01:27:02 +0300

    EX5

--- ex5.js
+++ ex5.js
@@ -11,22 +11,39 @@
 // (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 
 
-function getSum( a,b )
-{
-  let big,small;
-   if(a>b){
-    big = a;
-    small= b;
-   }else {
-    big = b;
-    small=a;
-   }
+// function getSum( a,b )
+// {
+//   let big,small;
+//    if(a>b){
+//     big = a;
+//     small= b;
+//    }else {
+//     big = b;
+//     small=a;
+//    }
 
-   for (let i =big ;i>small; i--) {
-    big += i-1;
+//    for (let i =big ;i>small; i--) {
+//     big += i-1;
     
-   }
-   return(big)
-};
+//    }
+//    return(big)
+// };
 
-console.log(getSum(a,b));
+// console.log(getSum(a,b));
+
+
+//! CODEWARS ÇÖZÜMÜ
+
+1)
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+      max = Math.max(a, b);
+  return (max - min + 1) * (min + max) / 2;
+}
+
+2)function GetSum( a,b )
+{
+   if (a == b) return a;
+   else if (a < b) return a + GetSum(a+1, b);
+   else return a + GetSum(a-1,b);
+}
