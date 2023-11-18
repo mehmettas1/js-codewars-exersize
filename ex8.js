e5d757f98843c5afaf7c88d95e61dcbf9457002d
mehmet <tas349873@gmail.com>
2023-11-18 13:18:40 +0300

    update

--- ex8.js
+++ ex8.js
@@ -1,3 +1,18 @@
+// let tas1 = String(123231312315)
+// console.log( tas );
+// console.log( tas1 );
+
+
+// let meta = "bu bir denemedir";
+
+// let two = meta.split("").reverse().join("")
+
+
+// let as = [1,2,3,4,5,8,2,4]
+
+// let asas =as.slice().sort((a, b) => parseInt(b) - parseInt(a)).join("")
+
+// console.log(asas);
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
 // Examples:
@@ -21,18 +36,4 @@ function descendingOrder(n) {
   }
   
  let tas =descendingOrder("123456789")
- let tas1 = String(123231312315)
-console.log( tas );
-console.log( tas1 );
-
-
-let meta = "bu bir denemedir";
-
-let two = meta.split("").reverse().join("")
- 
-
-let as = [1,2,3,4,5,8,2,4]
-
-let asas =as.slice().sort((a, b) => parseInt(b) - parseInt(a)).join("")
-
-console.log(asas);
+  console.log( tas );
