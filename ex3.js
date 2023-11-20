158974f26ab7cec314c952b461f71afde4ce3ab8
mehmet <tas349873@gmail.com>
2023-11-20 17:56:23 +0300

    update

--- ex3.js
+++ ex3.js
@@ -1,4 +1,55 @@
-//!Write a Python/JavaScript code to sort the list/array below without using .sort() method.
+// 1) !Write a Python/JavaScript code to sort the list/array below without using .sort() method.
 // elements of list = [999, 333, 2, 8982, 12, 45, 77, 99, 11]
 // Expected output:
 // [2, 11, 12, 45, 77, 99, 333, 999, 8982]
+
+// function selectionSort(arr) {
+//     const n = arr.length;
+//     for (let i = 0; i < n; i++) {
+//         let minIdx = i;
+//         for (let j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[minIdx]) {
+//                 minIdx = j;
+//             }
+//         }
+//         [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+//     }
+// }
+
+// // Test verisi
+// let elements = [999, 333, 2, 8982, 12, 45, 77, 99, 11];
+// selectionSort(elements);
+// console.log(elements); // Sıralanmış liste
+
+
+//2) Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+// For example (Input -> Output):
+
+// 2 -> 3 (1 + 2)
+// 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+
+// function summation(num) {
+//     let total = 0;
+//     for (let i = 1; i <= num; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+
+// Test verileri
+// console.log(summation(2)); // 3
+// console.log(summation(8)); // 36
+
+
+function meta(num) {
+    let total = 0;
+    for(let i =0;i<=num;i++) {
+        total +=i;
+    }
+
+    return total;
+}
+
+console.log(meta(121123))
--- ex8.js
+++ ex8.js
@@ -38,52 +38,52 @@ function descendingOrder(n) {
  let tas =descendingOrder("123456789")
   console.log( tas );
 //////////**************////////////****** */ */
-  function flickSwitch(arr) {
-    let shouldReturnTrue = true;
-    let result = [];
+//   function flickSwitch(arr) {
+//     let shouldReturnTrue = true;
+//     let result = [];
   
-    for (let item of arr) {
-      if (item === "flick") {
-        shouldReturnTrue = !shouldReturnTrue;
-      }
-      result.push(shouldReturnTrue);
-    }
+//     for (let item of arr) {
+//       if (item === "flick") {
+//         shouldReturnTrue = !shouldReturnTrue;
+//       }
+//       result.push(shouldReturnTrue);
+//     }
   
-    return result;
-  }
+//     return result;
+//   }
 
 
 
-console.log(flickSwitch(  ['codewars', 'flick', 'code', 'wars']))
+// console.log(flickSwitch(  ['codewars', 'flick', 'code', 'wars']))
   
 
-function detectOperator(a) {
-    var op = "no info";
+// function detectOperator(a) {
+//     var op = "no info";
     
-    var operatorCodes = {
-      '039': 'Golden Telecom',
-      '050': 'MTS',
-      '063': 'Life:)',
-      '066': 'MTS',
-      '067': 'Kyivstar',
-      '068': 'Beeline',
-      '093': 'Life:)',
-      '095': 'MTS',
-      '096': 'Kyivstar',
-      '097': 'Kyivstar',
-      '098': 'Kyivstar',
-      '099': 'MTS Test'
-    };
+//     var operatorCodes = {
+//       '039': 'Golden Telecom',
+//       '050': 'MTS',
+//       '063': 'Life:)',
+//       '066': 'MTS',
+//       '067': 'Kyivstar',
+//       '068': 'Beeline',
+//       '093': 'Life:)',
+//       '095': 'MTS',
+//       '096': 'Kyivstar',
+//       '097': 'Kyivstar',
+//       '098': 'Kyivstar',
+//       '099': 'MTS Test'
+//     };
   
-    var code = a.substring(1, 4);
-    if (operatorCodes.hasOwnProperty(code)) {
-      op = operatorCodes[code];
-    }
+//     var code = a.substring(1, 4);
+//     if (operatorCodes.hasOwnProperty(code)) {
+//       op = operatorCodes[code];
+//     }
   
-    return op;
-  }
+//     return op;
+//   }
   
 
-  console.log(detectOperator('805067123456')); // Çıktı: "Kyivstar"
-console.log(detectOperator('80991234567')); // Çıktı: "MTS"
-console.log(detectOperator('80123456789')); // Çıktı: "no info"
+//   console.log(detectOperator('805067123456')); // Çıktı: "Kyivstar"
+// console.log(detectOperator('80991234567')); // Çıktı: "MTS"
+// console.log(detectOperator('80123456789')); // Çıktı: "no info"
