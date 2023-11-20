cf7a853764a2eb0731fe98b98565e086f1da928d
mehmet <tas349873@gmail.com>
2023-11-20 18:03:32 +0300

    update

--- ex3.js
+++ ex3.js
@@ -42,3 +42,28 @@ console.log(summation(2)); // 3
 console.log(summation(8)); // 36
 
 
+// 3)  Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+    // Sayıyı dizeye dönüştürerek rakamları elde et
+    let digits = n.toString().split('');
+    
+    // Rakamları azalan sırada sırala
+    let sortedDigits = digits.sort((a, b) => parseInt(b) - parseInt(a));
+    
+    // Sıralı rakamları birleştirerek sayıya dönüştür
+    let result = parseInt(sortedDigits.join(''));
+    
+    return result;
+  }
+  
+ let tas =descendingOrder("123456789")
+  console.log( tas );
+
--- ex8.js
+++ ex8.js
@@ -13,30 +13,7 @@
 // let asas =as.slice().sort((a, b) => parseInt(b) - parseInt(a)).join("")
 
 // console.log(asas);
-// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
-// Examples:
-// Input: 42145 Output: 54421
-
-// Input: 145263 Output: 654321
-
-// Input: 123456789 Output: 987654321
-
-function descendingOrder(n) {
-    // Sayıyı dizeye dönüştürerek rakamları elde et
-    let digits = n.toString().split('');
-    
-    // Rakamları azalan sırada sırala
-    let sortedDigits = digits.sort((a, b) => parseInt(b) - parseInt(a));
-    
-    // Sıralı rakamları birleştirerek sayıya dönüştür
-    let result = parseInt(sortedDigits.join(''));
-    
-    return result;
-  }
-  
- let tas =descendingOrder("123456789")
-  console.log( tas );
 //////////**************////////////****** */ */
 //   function flickSwitch(arr) {
 //     let shouldReturnTrue = true;
