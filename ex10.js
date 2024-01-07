f4a8ccd830db8af5d88bd4152b53f6b64de01dc1
mehmet <tas349873@gmail.com>
2024-01-07 15:42:00 +0300

    ...

--- ex10.js
+++ ex10.js
@@ -1,46 +1,20 @@
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; şeklinde bir dizi verilmiştir. Bu dizideki çift sayıları bulun ve çift sayıların toplamını döndürün.
+// const fiyatlar = [100, 250, 50, 89];
 
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,24]
+// const yeniFiyatlar = fiyatlar.map((deger, indis) => {
+//   const yeniDeger = deger + (deger * 10 / 100);
+//   console.log(`${indis}. iterasyon: ${yeniDeger}`);
+//   return yeniDeger;
+// });
 
-// function sum(numbers) {
+// console.log(yeniFiyatlar);
 
-// let toplama = 0;
 
-// for (let i = 0; i < numbers.length; i++) {
-//   if(numbers[i] % 2 ==0) {
-    
-//     toplama += numbers[i];
-//   }
-  
-  
-// }
-// return toplama
+const fiyatlar  = [100,250 ,50 ,89];
 
-// }
+const yeniFiyatlar = fiyatlar.map((deger,index)=>{
+  const yeniDeger = deger+deger*10/100;
+  console.log(`${index}  ${deger} ==> yeni değeri ${yeniDeger}`)
+  return yeniDeger
+})
 
-// const meta = sum(numbers);
-
-// console.log(meta);
-
-function findDuplicates(arr) {
-    let counts = {};
-    let duplicates = [];
-  
-    for (let i = 0; i < arr.length; i++) {
-      let num = arr[i];
-      counts[num] = counts[num] ? counts[num] + 1 : 1;
-    }
-  
-    for (const num in counts) {
-      if (counts[num] > 1) {
-        duplicates.push(parseInt(num));
-      }
-    }
-  
-    return duplicates;
-  }
-  
-  const numbers = [4, 2, 4, 5, 2, 3, 1, 3];
-  const duplicates = findDuplicates(numbers);
-  console.log("Tekrar eden sayılar:", duplicates);
-  
+console.log(yeniFiyatlar);
