328700ccc56fe020253c3919494823e78dbae3e0
mehmet <tas349873@gmail.com>
2024-01-07 15:44:54 +0300

    ...

--- ex10.js
+++ ex10.js
@@ -9,7 +9,7 @@
 // console.log(yeniFiyatlar);
 
 
-const fiyatlar  = [100,250 ,50 ,89];
+const fiyatlar  = [100,250 ,50 ,89,555,650];
 
 const yeniFiyatlar = fiyatlar.map((deger,index)=>{
   const yeniDeger = deger+deger*10/100;
@@ -18,3 +18,10 @@ const yeniFiyatlar = fiyatlar.map((deger,index)=>{
 })
 
 console.log(yeniFiyatlar);
+
+
+ 
+//* fiyatlar array'inde fiyatı 250 TL den az olanlari ayri bir diziye saklayalim.
+const kucuk250 = fiyatlar.filter((f) => f<250)
+
+console.log(kucuk250);
