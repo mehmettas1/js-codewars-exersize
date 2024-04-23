506a4216468ce4485b21e05b0780342139bcf24c
mehmet <tas349873@gmail.com>
2024-04-23 19:04:35 +0300

    ...

---
+++ js-algoritms/easy.js
@@ -0,0 +1,3 @@
+import { userValidation } from "./easy/1_userValidation.js";
+
+userValidation("u_hello_word123")
---
+++ js-algoritms/easy/1_userValidation.js
@@ -0,0 +1,13 @@
+export const userValidation = (str)=> {
+    if
+
+}
+
+
+// 1. The username is between 4 and 25 characters.
+// 2. It must start with a letter.
+// 3. It can only contain letters, numbers, and the underscore character.
+// 4. It cannot end with an underscore character.
+
+
+// u_hello_word123
