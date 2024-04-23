f2f1431ac687ab1660c1bf713f0c00bad335c0e8
mehmet <tas349873@gmail.com>
2024-04-23 19:24:56 +0300

    It cannot end with an underscore character.

--- js-algoritms/easy/1_userValidation.js
+++ js-algoritms/easy/1_userValidation.js
@@ -1,6 +1,6 @@
 export const userValidation = (str) => {
     if (
-        str.length >= 4 && str.length <= 25 &&(/[a-zA-Z]/).test(str.slice(0,1)) && (/^\w+$/).test(str)
+        str.length >= 4 && str.length <= 25 &&(/[a-zA-Z]/).test(str.slice(0,1)) && (/^\w+$/).test(str) && (/^[a-zA-Z0-9]/).test(str.slice(-1))
     ) {
         return true
     } else {
